fcd6158f066e788beff90b236a69c0c6f05367cb
hilja <hiljaaa@gmail.com>
2014-07-10 19:09:56 +0200

    fix error in rename task

--- grunt/rename.js
+++ grunt/rename.js
@@ -1,6 +1,8 @@
 // rename
 // https://www.npmjs.org/package/grunt-rename
-renameNormalize: {
-    src: 'lib/normalize-css/normalize.css',
-    dest: 'lib/normalize-css/normalize.scss'
-}
+module.exports = function(grunt) {
+    renameNormalize: {
+        src: 'lib/normalize-css/normalize.css',
+        dest: 'lib/normalize-css/normalize.scss'
+    }
+};
